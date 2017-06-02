cf361923d0b23388e771ac27dc662bf926c241c3
Mayron Ceccon <mayron.ceccon@tecnorisk.com.br>
2017-06-02 19:37:10 +0000

    ajustes

--- bot.py
+++ bot.py
@@ -5,6 +5,6 @@ bot = telebot.TeleBot(os.environ['BOT_API_TOKEN'])
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, u"Olá, bem-vindo ao bot!")
+    bot.reply_to(message, u"Ola, bem-vindo ao bot!")
 
 bot.polling()
